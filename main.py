cf163a286a9395de993183a5838dc93cfc3490ec
Marloed <alexandr@Alexandrs-MacBook-Pro.local>
2026-05-26 12:17:43 +0300

    Update app responses and main setup

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from blacksheep import Application
 from dotenv import load_dotenv
-import os
 
 import app.misc_routes
 import app.users_routes
 
 load_dotenv()
-
-database_url = os.getenv("DATABASE_URL")
-
 app = Application()
